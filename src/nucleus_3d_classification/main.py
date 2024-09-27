79233845d6058f498d163be4b5b5518a2ebfb63b
Aurimas <144655945+agreic@users.noreply.github.com>
2024-09-27 18:43:21 +0200

    forgot a comma

--- src/nucleus_3d_classification/main.py
+++ src/nucleus_3d_classification/main.py
@@ -237,7 +237,7 @@ class FineTuneBatchSizeFinder(BatchSizeFinder):
 def define_trainer(args, callbacks=None):
     trainer_kwargs = {
         'profiler': args.profiler,
-        'enable_progress_bar': args.enable_progress_bar
+        'enable_progress_bar': args.enable_progress_bar,
         'max_epochs': args.max_epochs,
         'default_root_dir': args.default_root_dir,
         'devices': args.devices,
@@ -344,7 +344,7 @@ def parse_arguments():
     nn_common_parser.add_argument("--enable_progress_bar", action="store_false", help="Disables the progress bar")
     nn_common_parser.add_argument("--max_epochs", type=int, default=10, help="Max epochs for training")
     nn_common_parser.add_argument("--default_root_dir", type=str, default="./logs", help="Default root directory for logs")
-    nn_common_parser.add_argument("--devices", type=int, default=1, help="Devices to use for training")
+    nn_common_parser.add_argument("--devices", default='auto', help="Devices to use for training, expects int, default is auto")
     nn_common_parser.add_argument("--accelerator", type=str, default="auto", choices=['cpu', 'gpu', 'tpu'], help="Accelerator to use for training")
     nn_common_parser.add_argument("--accumulate_grad_batches", type=int, default=1, help="Accumulate gradient batches")
     nn_common_parser.add_argument("--fast_dev_run", action="store_true", help="Run a fast development run")
