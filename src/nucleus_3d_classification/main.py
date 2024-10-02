9ee46c39b821bffe38d13acb6461aa0697c29dfa
Aurimas <144655945+agreic@users.noreply.github.com>
2024-10-02 15:49:17 +0200

    debugv2

--- src/nucleus_3d_classification/main.py
+++ src/nucleus_3d_classification/main.py
@@ -532,13 +532,6 @@ def load_data_module(args):
     return data_module
 
 def train_nn_model(args):
-
-
-    # Same with other logger: #TODO DELETE THIS LATER
-    # Start recording memory snapshot history
-    start_record_memory_history()
-
-
     data_module = load_data_module(args)
     #print(f"train loader is type {type(data_module.train_dataloader())}")
     loss_fn = create_loss_fn_with_weights(data_module.train_dataloader(), loss_fn=args.loss_fn, weight=args.loss_weight)
@@ -562,17 +555,21 @@ def train_nn_model(args):
     # Start recording memory snapshot history
     # start_record_memory_history()
 
-    trainer.fit(model, datamodule=data_module)
 
-    # Create the memory snapshot file
-    export_memory_snapshot()
+    with torch.profiler.profile(
+       activities=[
+           torch.profiler.ProfilerActivity.CPU,
+           torch.profiler.ProfilerActivity.CUDA,
+       ],
+       schedule=torch.profiler.schedule(wait=0, warmup=0, active=6, repeat=1),
+       record_shapes=True,
+       profile_memory=True,
+       with_stack=True,
+       on_trace_ready=trace_handler,
+   ) as prof:
+        trainer.fit(model, datamodule=data_module)
 
-    # Stop recording memory snapshot history
-    stop_record_memory_history()
-
-
-
-########################################### LOGGER SET UP, DELETE LATER TODO TODO TODO
+#################### MEMORY PROFILING ####################
 logging.basicConfig(
    format="%(levelname)s:%(asctime)s %(message)s",
    level=logging.INFO,
@@ -583,44 +580,18 @@ logger.setLevel(level=logging.INFO)
 
 TIME_FORMAT_STR: str = "%b_%d_%H_%M_%S"
 
-# Keep a max of 100,000 alloc/free events in the recorded history
-# leading up to the snapshot.
-MAX_NUM_OF_MEM_EVENTS_PER_SNAPSHOT: int = 100000
-
-def start_record_memory_history() -> None:
-    if not torch.cuda.is_available():
-       logger.info("CUDA unavailable. Not recording memory history")
-       return
-
-    logger.info("Starting snapshot record_memory_history")
-    torch.cuda.memory._record_memory_history(
-       max_entries=MAX_NUM_OF_MEM_EVENTS_PER_SNAPSHOT
-   )
-
-def stop_record_memory_history() -> None:
-    if not torch.cuda.is_available():
-        logger.info("CUDA unavailable. Not recording memory history")
-        return
-
-    logger.info("Stopping snapshot record_memory_history")
-    torch.cuda.memory._record_memory_history(enabled=None)
-
-def export_memory_snapshot() -> None:
-    if not torch.cuda.is_available():
-       logger.info("CUDA unavailable. Not exporting memory snapshot")
-       return
+def trace_handler(prof: torch.profiler.profile):
    # Prefix for file names.
-    host_name = socket.gethostname()
-    timestamp = datetime.now().strftime(TIME_FORMAT_STR)
-    file_prefix = f"{host_name}_{timestamp}"
+   host_name = socket.gethostname()
+   timestamp = datetime.now().strftime(TIME_FORMAT_STR)
+   file_prefix = f"{host_name}_{timestamp}"
 
-    try:
-        logger.info(f"Saving snapshot to local file: {file_prefix}.pickle")
-        torch.cuda.memory._dump_snapshot(f"{file_prefix}.pickle")
-    except Exception as e:
-        logger.error(f"Failed to capture memory snapshot {e}")
-        return
-################################################################################################################
+   # Construct the trace file.
+   prof.export_chrome_trace(f"{file_prefix}.json.gz")
+
+   # Construct the memory timeline file.
+   prof.export_memory_timeline(f"{file_prefix}.html", device="cuda:0")
+#################### MEMORY PROFILING ####################
 
 def predict_nn_model(args):
     model_class = get_nn_model_class(args.model_class)
